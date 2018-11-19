915d2ceb5d044cfd6fde0b5464cffe56f0ae1126
Adam Jonas <jonas@flatironschool.com>
2018-11-18 20:37:01 -0500

    pytest

--- test/index_test.py
+++ test/index_test.py
@@ -1,12 +1,10 @@
 from unittest import TestCase
 from ipynb.fs.full.index import *
 
-class PointTest(TestCase):
-
-    def test_add0(self):
-        a = Point(x=None, y=None, a=5, b=7)
-        b = Point(x=2, y=5, a=5, b=7)
-        c = Point(x=2, y=-5, a=5, b=7)
-        self.assertEqual(a+b, b)
-        self.assertEqual(b+a, b)
-        self.assertEqual(b+c, a)
+def test_add0(self):
+    a = Point(x=None, y=None, a=5, b=7)
+    b = Point(x=2, y=5, a=5, b=7)
+    c = Point(x=2, y=-5, a=5, b=7)
+    assert a+b == b
+    assert b+a == b
+    assert b+c == a
